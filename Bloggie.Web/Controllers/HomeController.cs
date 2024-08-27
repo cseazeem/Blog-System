cfb19a645591092d310a5e7fc064b1e82bcac008
cseazeem <azeema224143@gmail.com>
2024-08-27 10:04:35 +0530

    My Blog System

---
+++ Bloggie.Web/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using Bloggie.Web.Models;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Bloggie.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IBlogPostRepository blogPostRepository;
+        private readonly ITagRepository tagRepository;
+
+        public HomeController(ILogger<HomeController> logger,
+            IBlogPostRepository blogPostRepository,
+            ITagRepository tagRepository
+            )
+        {
+            _logger = logger;
+            this.blogPostRepository = blogPostRepository;
+            this.tagRepository = tagRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // getting all blogs
+            var blogPosts = await blogPostRepository.GetAllAsync();
+
+            // get all tags
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new HomeViewModel
+            {
+                BlogPosts = blogPosts,
+                Tags = tags
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Process the form submission
+                // This is where you would handle the contact form submission logic
+
+                // Redirect to a confirmation page upon successful submission
+                return RedirectToAction("ContactConfirmation");
+            }
+            else
+            {
+                // If the model is not valid, redisplay the contact form with validation errors
+                return View("Contact", model);
+            }
+        }
+
+        public IActionResult ContactConfirmation()
+        {
+            return View();
+        }
+        [HttpGet] // This is important to ensure the Contact view is rendered via a GET request
+        public IActionResult Contact()
+        {
+            var model = new ContactViewModel();
+            return View(model);
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
